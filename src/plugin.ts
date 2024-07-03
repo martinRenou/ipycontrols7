60dc600beae890937dadf1cf2f6d25a21fa6d5bd
martinRenou <martin.renou@gmail.com>
2024-07-03 17:03:26 +0200

    First commit

---
+++ src/plugin.ts
@@ -0,0 +1,41 @@
+// Copyright (c) martinRenou
+// Distributed under the terms of the Modified BSD License.
+
+import { Application, IPlugin } from '@lumino/application';
+
+import { Widget } from '@lumino/widgets';
+
+import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
+
+import { JUPYTER_CONTROLS_VERSION } from '@jupyter-widgets/controls/lib/version';
+import * as widgetsExport from '@jupyter-widgets/controls';
+
+const EXTENSION_ID = 'ipycontrols7:plugin';
+
+/**
+ * The example plugin.
+ */
+const controlsPlugin: IPlugin<Application<Widget>, void> = {
+  id: EXTENSION_ID,
+  requires: [IJupyterWidgetRegistry],
+  activate: activateWidgetExtension,
+  autoStart: true,
+} as unknown as IPlugin<Application<Widget>, void>;
+// the "as unknown as ..." typecast above is solely to support JupyterLab 1
+// and 2 in the same codebase and should be removed when we migrate to Lumino.
+
+export default controlsPlugin;
+
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(
+  app: Application<Widget>,
+  registry: IJupyterWidgetRegistry
+): void {
+  registry.registerWidget({
+    name: '@jupyter-widgets/controls',
+    version: JUPYTER_CONTROLS_VERSION,
+    exports: widgetsExport as unknown as any
+  });
+}
